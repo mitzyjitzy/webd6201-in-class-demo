eff5c74ace6bb93cac26c0141bbf76db1b30763c
mitzyjitzy <mikesterjetster@gmail.com>
2023-01-29 16:12:47 -0500

    Added everything from ICE-03

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,43 @@
 (function () {
-    function Start() {
+
+function DisplayButton() {
+let RandomButton = document.getElementById("RandomButton")
+RandomButton.addEventListener("click", function() {
+location.href = './projects.html'
+
+})
+
+let mainContent = document.getElementsByTagName("main")[0]
+
+let mainParagraph = document.createElement("p")
+mainParagraph.setAttribute("id", "MainParagraph")
+mainParagraph.setAttribute("class", "mt-3")
+let firstString = "This is a paragraph you are seeing thanks to JavaScript."
+mainParagraph.textContent = firstString
+
+
+mainContent.appendChild(mainParagraph)
+
+
+console.log(mainContent)
+
+}
+
+
+function Start() { 
         console.log("App Started!")
-    }
+
+       
+        switch (document.title) {
+            case "Home - WEBD6201 Demo":
+                DisplayButton()
+                break
+            case "Projects - WEBD6201 Demo":
+                DisplayButton()
+                break
+        }
+
+    }   
 
     window.addEventListener("load", Start)
 })()
